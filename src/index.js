3c76ceb08d8b16a7d2f230fe1a1d7775206afd14
Mary <maryhoglan78@gmail.com>
2025-07-03 15:54:15 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import { setupServer } from './server.js';
+import 'dotenv/config';
+import { initMongoConnection } from './db/initMongoConnection.js';
+
+
+
+async function main() {
+  try {
+    await initMongoConnection(); 
+    setupServer();               
+  } catch (error) {
+    console.error('Failed to start app:', error.message);
+    process.exit(1);
+  }
+}
+
+main();
--- src/server.js
+++ src/server.js
@@ -1,2 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+import { getContacts } from './controllers/contacts.js';
+import { getContactByIdController } from './controllers/contacts.js';
 
 
+export function setupServer() {
+    const app = express();
+
+    app.use(cors());
+    app.use(pino());
+
+
+    app.get('/contacts', getContacts);
+    app.get('/contacts/:contactId', getContactByIdController);
+ 
+    app.get('/', (req, res) => {
+        res.json({ message: 'API is running' });
+    });
+
+
+    app.use((req, res) => {
+        res.status(404).json({ message: 'Not found' });
+    });
+
+const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+
+  return app;
+
+    
+}
