f00c1ade1112ced4b2a3867a91a6bffa9f5ed03e
Mary <maryhoglan78@gmail.com>
2025-07-22 17:53:54 +0300

    Initial commit

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,7 +1,53 @@
 import { Contact } from '../models/contact.js';
 
-export async function getAllContacts() {
-  return await Contact.find({});
+
+const calculatePaginationData = (count, page, perPage) => {
+  const totalPages = Math.ceil(count / perPage);
+  const hasNextPage = page < totalPages;
+  const hasPreviousPage = page > 1;
+
+  return {
+    page,
+    perPage,
+    totalItems: count,
+    totalPages,
+    hasNextPage,
+    hasPreviousPage,
+  };
+};
+
+export async function getAllContacts(
+  page,
+  perPage,
+  sortBy,
+  sortOrder,
+  filter
+) {
+  const skip = page > 0 ? (page - 1) * perPage : 0;
+  const limit = perPage;
+
+  const query = Contact.find();
+
+  if (typeof filter?.type !== 'undefined') {
+    query.where('contactType').equals(filter.type);
+  }
+
+  if (typeof filter?.isFavourite !== 'undefined') {
+    query.where('isFavourite').equals(filter.isFavourite);
+  }
+
+  const [totalCount, contacts] = await Promise.all([
+    query.clone().countDocuments(),
+    query
+      .sort({ [sortBy]: sortOrder })
+      .skip(skip)
+      .limit(limit)
+      .exec(),
+  ]);
+
+  const paginationData = calculatePaginationData(totalCount, page, perPage);
+
+  return { data: contacts, ...paginationData };
 }
 
 export async function getContactById(id) {
@@ -9,7 +55,7 @@ export async function getContactById(id) {
 }
 
 export async function createContact(payload) {
-  return Contact.create(payload); 
+  return Contact.create(payload);
 }
 
 export async function updateContact(id, payload) {
